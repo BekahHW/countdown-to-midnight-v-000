c768e5743d6e145ce22d16018af6ea740741ea87
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-04 10:41:57 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -13,8 +13,9 @@ end
 def countdown_with_sleep(number)
   number= 10
   while number > 10
-  puts "#{number} SECOND(S)!"
-  number -=1
-  return "Happy Sleep"
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+   number -=1
 end
+  return "HAPPY NEW YEAR!"
 end
